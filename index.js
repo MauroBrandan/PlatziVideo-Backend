81ab977080ac3c0846c813c32b9aaf1276a5e22b
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-07-30 21:50:05 -0300

    Implementando boom y error 404

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const { config } = require('./config/index')
 const moviesApi = require('./routes/movies')
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers')
+const {
+	logErrors,
+	errorHandler,
+	wrapErrors,
+} = require('./utils/middleware/errorHandlers')
+const notFoundHandler = require('./utils/middleware/notFoundHandler')
 
 const app = express()
-app.use(express.json())
+app.use(express.json()) // Body parser
 
+// Routes
 moviesApi(app)
 
+// Catch 404
+app.use(notFoundHandler)
+
+// Errors middleware
 app.use(logErrors)
+app.use(wrapErrors)
 app.use(errorHandler)
 
 app.listen(config.port, () => {
