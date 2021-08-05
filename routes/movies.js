6fe956f031fb23b5de65f3da5ed0fece14eccc73
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-08-05 12:25:05 -0300

    Implementando una capa de manejo de cache

--- routes/movies.js
+++ routes/movies.js
@@ -6,6 +6,11 @@ const {
 	updateMovieSchema,
 } = require('../utils/schemas/movies')
 const validationHandler = require('../utils/middleware/validationHandler')
+const cacheResponse = require('../utils/cacheResponse')
+const {
+	FIVE_MINUTES_IN_SECONDS,
+	SIXTY_MINUTES_IN_SECONDS,
+} = require('../utils/time')
 
 function moviesApi(app) {
 	const router = express.Router()
@@ -14,6 +19,7 @@ function moviesApi(app) {
 	app.use('/api/movies', router)
 
 	router.get('/', async (req, res, next) => {
+		cacheResponse(res, FIVE_MINUTES_IN_SECONDS)
 		const { tags } = req.query
 
 		try {
@@ -32,6 +38,7 @@ function moviesApi(app) {
 		'/:movieId',
 		validationHandler({ movieId: movieIdSchema }, 'params'),
 		async (req, res, next) => {
+			cacheResponse(res, SIXTY_MINUTES_IN_SECONDS)
 			const { movieId } = req.params
 
 			try {
